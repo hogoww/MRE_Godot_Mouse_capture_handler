0f944d43329eee7ab984de997f9b684d594a0252
Hogoww <pierre_misse25@msn.com>
2026-09-09 15:34:50 +0200

    [improvement] remove unused variable, try to clear up a comment

--- _input_event.gd
+++ _input_event.gd
@@ -1,9 +1,5 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 func _input_event(_camera: Camera3D, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
